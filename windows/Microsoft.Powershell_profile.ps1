ff7eb5a176897c388f5d5df5e70889ef3da04546
Apurv Kumaria <kumaria.apurv@gmail.com>
2012-01-16 11:24:03 -0800

    Adding prompt function

--- windows/Microsoft.Powershell_profile.ps1
+++ windows/Microsoft.Powershell_profile.ps1
@@ -27,6 +27,17 @@ function shorten-path([string] $path) {
    return ($loc -replace '\\(\.?)([^\\])[^\\]*(?=\\)','\$1$2') 
 }
 
+function prompt { 
+   $green = [ConsoleColor]::Green 
+   $blue = [ConsoleColor]::Blue
+
+   write-host "$([char]0x1A9) " -n -f $green
+   #write-host ([net.dns]::GetHostName()) -n -f $chost 
+   write-host (shorten-path (pwd).Path) -n -f $blue
+   write-host " $([char]0x0BB)" -n -f $green
+   return ' ' 
+}
+
 function pp {
     cd C:\work\1953\e3\components\omt\bundles\oms\pOMS.main
 }
